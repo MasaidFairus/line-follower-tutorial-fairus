ef200dd25a9c4924971fd4e3f519c38b2942ea41
Masaid Fairus Trimarsongko <106062448+MasaidFairus@users.noreply.github.com>
2023-02-10 14:12:44 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.showLeds(`
+    . # # # .
+    . . # . .
+    # # # # #
+    # . # . #
+    # . . . #
+    `)
+Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+basic.forever(function () {
+    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 70)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Left, 50)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Right, 50)
+    } else {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Stop, 0)
+    }
+})
